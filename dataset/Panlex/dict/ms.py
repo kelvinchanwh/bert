0c89e17e8e73a6d73e1605f306291a0dfa2d811e
kelvinchanwh <35751887+kelvinchanwh@users.noreply.github.com>
2021-04-28 20:08:09 +0800

    include panlex dataset

---
+++ dataset/Panlex/dict/ms.py
@@ -0,0 +1,14 @@
+with open ("./ms2_out.txt") as ms:
+    with open ("./ms2_out1.txt", 'w') as writer:
+        for line in ms:
+            # replace_list = ["basic_meanings", "military", "capital_city", "plant", "marketing", "ecology", \
+            #     "mechanical_engineering", "manufacturing", "statistics", "zoology", "real_estate", "house", \
+            #         "transportation", "management", "insurance", "construction", "botany", "chemistry", "logistics"\
+            #             "biology"]
+            # for word in replace_list:
+            #     line = line.replace(word, "\n")
+            # writer.write(line)
+            if line 
+            
+
+    
